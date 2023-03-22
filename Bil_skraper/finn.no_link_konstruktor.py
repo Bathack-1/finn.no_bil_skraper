dabf3bb0cd66bc403a76b9afddb096fa765f4b72
Bathack <batgamebatgame@gmail.com>
2023-03-22 07:46:52 +0100

    Gjøre det lettere å søke etter andre biler

---
+++ Bil_skraper/finn.no_link_konstruktor.py
@@ -0,0 +1,16 @@
+"""
+
+ordbøker med "link koden" til til forskjellige parametere:
+ordbok for de "viktigste":
+    merke
+    model
+    år
+    pris
+    km_stand
+    område
+    drivstoff
+
+
+få python til å skrive classene selv
+
+"""
