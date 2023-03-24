32ce3c0ace0c55d450771e2166d19b40559dc7bc
Bathack <batgamebatgame@gmail.com>
2023-03-25 00:05:14 +0100

    kjøre skrapeverktøyet

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from Bil_skraper.Skraper import Skraper
-from Bil_skraper.utviklings_verktøy import skrape_merker_verdi
+from Bil_skraper.utviklings_verktøy import skrape_merker_verdi, skrape_model_verdi
 
 def main():
     #skrap()
     #skraper = Skraper("https://www.finn.no/car/used/search.html?location=22030&location=20061&make=0.8078&mileage_to=200000&price_to=230000&sales_form=1&sort=PUBLISHED_DESC")
-    skrape_merker_verdi()
-
+    #skrape_merker_verdi()
+    skrape_model_verdi()
 
 if __name__ == '__main__':
     main()
