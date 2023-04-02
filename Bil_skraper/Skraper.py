98cf4b9d93939e79cf6754fd39c76f229f8e6c19
Håvard Bønes <haavard.bones@gmail.com>
2023-04-02 12:24:21 +0200

    session basert skraping

--- Bil_skraper/Skraper.py
+++ Bil_skraper/Skraper.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 
 class Skraper:
     def __init__(self, link):
+        self.session = requests.Session()
         self.link = link
         self.alle_biler = []
         self.spesifikasjons_ord = ["Farge", "Chassis", "Effekt", "Batteri",
@@ -54,7 +55,7 @@ class Skraper:
         return ordbok
 
     def skrap_hoved_siden(self):
-        side = requests.get(self.link)
+        side = self.session.get(self.link)
         html_data = BeautifulSoup(side.content, "html.parser")
 
         alle_annonser = html_data.find_all("article", class_="ads__unit")
@@ -98,7 +99,7 @@ class Skraper:
             bil.lokasjon = steds_vindu.text
 
     def skrap_annonse(self, bil):
-        side = requests.get(bil.link)
+        side = self.session.get(bil.link)
         html_data = BeautifulSoup(side.content, "html.parser")
 
         bil.beskrivelse = html_data.find(id="collapsableTextContent")  # finne beskrivelsen
